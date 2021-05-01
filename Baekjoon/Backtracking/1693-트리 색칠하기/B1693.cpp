084350793ce7c13894ccb8452902bc481d980205
w0nee <kwpark96@gmail.com>
2021-05-01 22:36:28 +0900

    21-05-01

---
+++ Baekjoon/Backtracking/1693-트리 색칠하기/B1693.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <map>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define INF 987654321
+
+int N;
+vector<int> graph[100010];
+vector<int> tree[100010];
+int dp[100010][20];
+int Min = INF;
+
+void makeTree(int before, int now)
+{
+	for(int i = 0; i < graph[now].size(); i++)
+	{
+		int next = graph[now][i];
+		
+		if(before != next)
+		{
+			tree[now].push_back(next);
+			makeTree(now, next);
+		}
+	}
+}
+
+int solve(int before, int now, int color)
+{
+	if(dp[now][color] != 0)
+	{
+		return dp[now][color];
+	}
+	
+	int colorSum = 0;
+	
+	for(int i = 0; i < graph[now].size(); i++)
+	{
+		int next = graph[now][i];
+		int Min = INF;
+		
+		if(before != next)
+		{
+			for(int nextColor = 1; nextColor <= 18; nextColor++)
+			{
+				if(color != nextColor)
+				{
+					Min = min(Min, solve(now, next, nextColor) + nextColor);
+				}
+			}	
+			
+			colorSum += Min;
+		}
+	}
+	
+	return dp[now][color] = colorSum;
+}
+
+int main(void)
+{
+//	freopen("B1693_input.txt", "r", stdin);
+
+	cin >> N;
+	
+	for(int i = 1; i <= N-1; i++)
+	{
+		int from, to;
+		cin >> from >> to;
+		
+		graph[from].push_back(to);
+		graph[to].push_back(from);
+	}
+	
+	makeTree(0, 1);
+	
+	for(int i = 1; i <= 18; i++)
+	{
+		Min = min(Min, solve(0, 1, i) + i);
+	}
+	
+	cout << Min;
+	
+	return 0;
+}
