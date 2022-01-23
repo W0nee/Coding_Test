a026ed750f54b186c2457319839c3da43ac473e0
W0nee <kwpark96@gmail.com>
2022-01-23 23:40:12 +0900

    22-01-23

--- Baekjoon/Sub-Array/2015-수들의 합4/B2015.cpp
+++ Baekjoon/Sub-Array/2015-수들의 합4/B2015.cpp
@@ -1,17 +1,27 @@
+#include <stdio.h>
 #include <iostream>
+#include <queue>
+#include <stack>
 #include <map>
+#include <string>
+#include <string.h>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+#define INF 987654321
+
 int N, K;
-int psum[200010];
 int num[200010];
+int psum[200010];
 map<int, long long> check;
-long long ans;
 
 int main(void)
 {
+//	freopen("B2015_input.txt", "r", stdin);
+	
+	long long ans = 0;
+	
 	cin >> N >> K;
 	
 	for(int i = 1; i <= N; i++)
@@ -27,15 +37,14 @@ int main(void)
 		{
 			ans++;
 		}
-		
-		int val = psum[i] - K;
-		
-		ans += check[val];
+
+		// psum[i] - psum[j] = K
+		ans += check[psum[i] - K];
 		
 		check[psum[i]]++;
 	}
 	
-	cout << ans << endl;
+	cout << ans;
 	
 	return 0;
 }
